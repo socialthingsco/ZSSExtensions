031bf6865525dd8804e50367a245ec05d644232b
Zachary Shakked <zach@shakd.io>
2019-01-22 14:08:48 -0500

    Added support for new devices

--- ZSSExtensions/Classes/UIDevice+Extensions.swift
+++ ZSSExtensions/Classes/UIDevice+Extensions.swift
@@ -10,37 +10,43 @@ import UIKit
 
 public extension UIDevice {
 
-    public var iPhoneX: Bool {
+    var iPhoneX: Bool {
         return UIScreen.main.nativeBounds.height == 2436
     }
-    public var iPhone: Bool {
+    var iPhone: Bool {
         return UIDevice.current.userInterfaceIdiom == .phone
     }
-    public enum ScreenType: String {
-        case iPhone4_4S = "iPhone 4 or iPhone 4S"
+    enum ScreenType: String {
+        case iPhones_4_4S = "iPhone 4 or iPhone 4S"
         case iPhones_5_5s_5c_SE = "iPhone 5, iPhone 5s, iPhone 5c or iPhone SE"
         case iPhones_6_6s_7_8 = "iPhone 6, iPhone 6S, iPhone 7 or iPhone 8"
         case iPhones_6Plus_6sPlus_7Plus_8Plus = "iPhone 6 Plus, iPhone 6S Plus, iPhone 7 Plus or iPhone 8 Plus"
-        case iPhoneX = "iPhone X"
+        case iPhones_X_XS = "iPhone X or iPhone XS"
+        case iPhone_XR = "iPhone XR"
+        case iPhone_XSMax = "iPhone XS Max"
         case unknown
     }
-    
-    public var screenType: ScreenType {
+    var screenType: ScreenType {
         switch UIScreen.main.nativeBounds.height {
         case 960:
-            return .iPhone4_4S
+            return .iPhones_4_4S
         case 1136:
             return .iPhones_5_5s_5c_SE
         case 1334:
             return .iPhones_6_6s_7_8
+        case 1792:
+            return .iPhone_XR
         case 1920, 2208:
             return .iPhones_6Plus_6sPlus_7Plus_8Plus
         case 2436:
-            return .iPhoneX
+            return .iPhones_X_XS
+        case 2688:
+            return .iPhone_XSMax
         default:
             return .unknown
         }
     }
+
     
         
     public static let modelName: String = {
