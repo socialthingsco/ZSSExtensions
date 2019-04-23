5437bd93342b60d34e7c6ccaa94eb9c1d4c0946c
Serhii Kharauzov <serhii.kharauzov@gmail.com>
2019-04-23 17:35:56 +0300

    Extend HapticFeedback class

--- ZSSExtensions/Classes/HapticFeedback.swift
+++ ZSSExtensions/Classes/HapticFeedback.swift
@@ -9,12 +9,11 @@
 import UIKit
 
 public final class HapticFeedback {
-    public static let shared: HapticFeedback = HapticFeedback()
     
+    public static let shared: HapticFeedback = HapticFeedback()
     public let selection: UISelectionFeedbackGenerator
     public let impact: UIImpactFeedbackGenerator
     public let notification: UINotificationFeedbackGenerator
-    
     
     init() {
         selection = UISelectionFeedbackGenerator()
@@ -25,6 +24,35 @@ public final class HapticFeedback {
         
         notification = UINotificationFeedbackGenerator()
         notification.prepare()
+    }
+    
+    // MARK: Public type methods - Haptic Feedback
+    
+    /// Performs haptic feedback - selection.
+    public static func performFeedbackSelection() {
+        if #available(iOS 10.0, *) {
+            let feedbackGenerator = UISelectionFeedbackGenerator()
+            feedbackGenerator.prepare()
+            feedbackGenerator.selectionChanged()
+        }
+    }
+    
+    /// Performs haptic feedback - impact.
+    public static func performFeedbackImpact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        if #available(iOS 10.0, *) {
+            let feedbackGenerator = UIImpactFeedbackGenerator(style: style)
+            feedbackGenerator.prepare()
+            feedbackGenerator.impactOccurred()
+        }
+    }
+    
+    /// Performs haptic feedback - notification.
+    public static func performFeedbackNotification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        if #available(iOS 10.0, *) {
+            let feedbackGenerator = UINotificationFeedbackGenerator()
+            feedbackGenerator.prepare()
+            feedbackGenerator.notificationOccurred(type)
+        }
     }
 }
 
